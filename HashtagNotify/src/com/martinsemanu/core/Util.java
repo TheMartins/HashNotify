f0000d6200c8343ae712ce469ce6759d46c3e8f8
Bravodin <manuelbravoc@gmail.com>
2013-02-02 00:29:32 -0200

    primeiro teste de pesquisa twitter

--- HashtagNotify/src/com/martinsemanu/core/Util.java
+++ HashtagNotify/src/com/martinsemanu/core/Util.java
@@ -1,5 +1,15 @@
 package com.martinsemanu.core;
 
-public class Util {
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
+public class Util {
+	
+	public static void PesquisarTwits(){
+		JSONParser jParser = new JSONParser();
+		JSONObject json = jParser.getJsonFromUrl("http://search.twitter.com/search.json?q=cpbr6");
+		
+		
+	}
 }
